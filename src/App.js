1961b26db96b0931531cc9a4329511012b4d2076
Anthony Tamayo Ortega <anthonytamayoortega@MacBook-Pro-de-Anthony.local>
2021-05-17 22:08:16 -0500

    i add a logo

--- src/App.js
+++ src/App.js
@@ -1,11 +1,13 @@
 import React from 'react'
 import { ListOfCategories } from './components/ListOfCategories'
-import { GlobalStyle } from './Global.styled'
+import { GlobalStyle } from './styles/Global.styled'
 import { ListOfPhotoCards } from './components/ListOfPhotoCards'
+import { Logo } from './components/Logo'
 
 export const App = () => (
   <div>
     <GlobalStyle />
+    <Logo />
     <ListOfCategories />
     <ListOfPhotoCards />
   </div>
--- src/components/ListOfCategories/index.js
+++ src/components/ListOfCategories/index.js
@@ -1,12 +1,12 @@
 import React from 'react'
 import { Category } from '../Category'
 import { List, Item } from './ListOfCategories.styled'
-import { categories } from '../../../api/db.json'
+import { categories as mockCategories } from '../../../api/db.json'
 
 export const ListOfCategories = () => {
   return (
     <List>
-      {categories.map((category) => (
+      {mockCategories.map((category) => (
         <Item key={category.id}>
           <Category {...category} />
         </Item>
