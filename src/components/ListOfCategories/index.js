fef51b9748b2715b47e87d56d23161beeafe8076
Anthony Tamayo Ortega <anthonytamayoortega@MacBook-Pro-de-Anthony.local>
2021-05-18 22:06:07 -0500

    i add changes on the list of categories

--- src/components/ListOfCategories/index.js
+++ src/components/ListOfCategories/index.js
@@ -3,10 +3,12 @@ import { Category } from '../Category'
 import { List, Item } from './ListOfCategories.styled'
 // import { categories as mockCategories } from '../../../api/db.json'
 
-export const ListOfCategories = () => {
+function useCategoriesData () {
   const [categories, setCategories] = useState([])
+  const [loading, setLoading] = useState(false)
 
   useEffect(() => {
+    setLoading(true)
     window
       .fetch(
         'https://curso-platzi-react-avanzado-83amsx1hz-atamayo1.vercel.app/categories'
@@ -14,16 +16,43 @@ export const ListOfCategories = () => {
       .then((res) => res.json())
       .then((response) => {
         setCategories(response)
+        setLoading(false)
       })
   }, [])
 
-  return (
-    <List>
-      {categories.map((category) => (
-        <Item key={category.id}>
-          <Category {...category} />
-        </Item>
-      ))}
+  return { categories, loading }
+}
+
+export const ListOfCategories = () => {
+  const { categories, loading } = useCategoriesData()
+  const [showFixed, setShowFixed] = useState(false)
+
+  useEffect(() => {
+    const onScroll = (e) => {
+      const newShowFixed = window.scrollY > 200
+      showFixed !== newShowFixed && setShowFixed(newShowFixed)
+    }
+    document.addEventListener('scroll', onScroll)
+
+    return () => document.removeEventListener('scroll', onScroll)
+  })
+
+  const renderList = (fixed) => (
+    <List fixed={fixed}>
+      {
+      loading ? <Item key='loading'><Category /></Item> : categories.map((category) => <Item key={category.id}><Category {...category} /></Item>)
+      }
     </List>
+  )
+
+  if (loading) {
+    return 'Cargando...'
+  }
+
+  return (
+    <>
+      {renderList()}
+      {showFixed && renderList(true)}
+    </>
   )
 }
