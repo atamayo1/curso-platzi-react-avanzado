b4d2f071e99c380191a0e9acc0309d77f6427d65
Anthony Tamayo Ortega <anthonytamayoortega@MacBook-Pro-de-Anthony.local>
2021-05-24 00:25:57 -0500

    i add a list of photocards with graphql

---
+++ src/components/ListOfPhotoCards/ListOfPhotoCards.styled.js
@@ -0,0 +1,6 @@
+import styled from 'styled-components'
+
+export const ContentSpinner = styled.div`
+    display: flex;
+    justify-content: center;
+`
--- src/components/ListOfPhotoCards/index.js
+++ src/components/ListOfPhotoCards/index.js
@@ -1,13 +1,37 @@
 import React from 'react'
 import { PhotoCard } from '../PhotoCard'
-import { photos } from '../../../api/db.json'
+import { useQuery, gql } from '@apollo/client'
+import { ReactLoading } from 'react-loading'
+import { ContentSpinner } from './ListOfPhotoCards.styled'
+// import { photos } from '../../../api/db.json'
+
+const withPhotos = gql`
+  query getPhotos {
+    photos {
+      id
+      categoryId
+      src
+      likes
+      liked
+      userId
+    }
+  }
+`
 
 export const ListOfPhotoCards = () => {
+  const { loading, error, data } = useQuery(withPhotos)
+  // console.log(data)
+  if (error) {
+    return <h2>Internal Server Error</h2>
+  }
+  if (loading) {
+    return <ContentSpinner><ReactLoading type='spin' color='#1b1b1b' /></ContentSpinner>
+  }
   return (
-    <div>
-      {photos.map((photo) => (
+    <>
+      {data.photos.map((photo) => (
         <PhotoCard key={photo.id} {...photo} />
       ))}
-    </div>
+    </>
   )
 }
