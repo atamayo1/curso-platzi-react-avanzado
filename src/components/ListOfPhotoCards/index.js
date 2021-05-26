fd525276fff5570b0b40033b5d5e48d48fb8fc00
Anthony Tamayo Ortega <anthonytamayoortega@MacBook-Pro-de-Anthony.local>
2021-05-26 00:35:58 -0500

    i add changes on the Categories and Photo Cards

--- src/components/ListOfPhotoCards/index.js
+++ src/components/ListOfPhotoCards/index.js
@@ -1,37 +1,28 @@
 import React from 'react'
 import { PhotoCard } from '../PhotoCard'
-import { useQuery, gql } from '@apollo/client'
-import { ReactLoading } from 'react-loading'
-import { ContentSpinner } from './ListOfPhotoCards.styled'
+import { useQuery } from '@apollo/client'
+import { GET_PHOTOS } from '../../hoc/getPhotos'
+// import { ReactLoading } from 'react-loading'
+// import { ContentSpinner } from './ListOfPhotoCards.styled'
 // import { photos } from '../../../api/db.json'
 
-const withPhotos = gql`
-  query getPhotos {
-    photos {
-      id
-      categoryId
-      src
-      likes
-      liked
-      userId
-    }
-  }
-`
-
-export const ListOfPhotoCards = () => {
-  const { loading, error, data } = useQuery(withPhotos)
-  // console.log(data)
+export const ListOfPhotoCards = ({ categoryId }) => {
+  // console.log('categoryId prop', categoryId)
+  const { loading, error, data } = useQuery(GET_PHOTOS, {
+    variables: { categoryId }
+  })
+  // console.log('data photos', data)
   if (error) {
     return <h2>Internal Server Error</h2>
   }
   if (loading) {
-    return <ContentSpinner><ReactLoading type='spin' color='#1b1b1b' /></ContentSpinner>
+    return 'Cargando Photo Cards...'
   }
   return (
-    <>
+    <div>
       {data.photos.map((photo) => (
         <PhotoCard key={photo.id} {...photo} />
       ))}
-    </>
+    </div>
   )
 }
